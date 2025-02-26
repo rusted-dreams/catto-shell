5094a4a175cb8ea92e620d88086c6169718b85a9
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2025-02-26 14:22:54 +0530

    implement echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -9,11 +10,20 @@ int main() {
   std::cout << "$ ";
 
   std::string input;
-  // std::getline(std::cin, input);
   while (true) {
     std::getline(std::cin, input);
-    if (input == "exit 0") break;
-    std::cout << input << ": "  << "command not found\n";
+    std::istringstream iss(input);
+    std::string command;
+    iss >> command;
+    if(command == "echo"){
+      std::string argument;
+      std::getline(iss, argument);
+      std::cout << argument << std::endl;
+    }
+    else if (input == "exit 0") break;
+    else {
+      std::cout << input << ": "  << "command not found\n";
+    }
     std::cout << "$ ";
   }
 }
