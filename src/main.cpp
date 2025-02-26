22a3a7ed005eaad5607a7b2bf7139d7a877bbcca
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2025-02-26 13:47:51 +0530

    take command inputs

--- src/main.cpp
+++ src/main.cpp
@@ -10,4 +10,10 @@ int main() {
 
   std::string input;
   std::getline(std::cin, input);
+  std::cout << input << ": "  << "command not found\n";
+  // while (true) {
+  //   std::getline(std::cin, input);
+  //   if (input == "") break;
+  //   std::cout << "$ ";
+  // }
 }
