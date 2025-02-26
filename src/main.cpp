1aed6a35e2ac688e8ac22d80251404f1882d6851
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2025-02-26 17:28:36 +0530

    add type command to look for binaries in given path env var

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <sstream>
+#include <filesystem>
 #include <unordered_set>
+
+
+void getCommand(const std::string paths, std::string arg){
+  std::string dir;
+  std::stringstream ss(paths);
+  
+  while(std::getline(ss, dir, ':')){
+
+    // search for the command in each dir from the path env var.
+    for(const auto& entry: std::filesystem::directory_iterator(dir)){
+      if(entry.path().filename() == arg) {
+        std::string command = entry.path();
+        std::cout << arg << " is " << command << std::endl;
+        return;
+      }
+    }
+  }
+  std::cout << arg << ": not found" << std::endl;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
   // Uncomment this block to pass the first stage
   std::cout << "$ ";
 
@@ -23,12 +42,17 @@ int main() {
       std::getline(iss >> std::ws, argument);
       std::cout << argument << std::endl;
     }
+
     else if(command == "type"){
       std::string arg;
       std::getline(iss >> std::ws, arg);
       if(commands.find(arg) != commands.end()) 
         std::cout << arg << " is a shell builtin" << std::endl;
-      else std::cout << arg << ": not found\n";
+      else {
+        std::string paths = std::getenv("PATH");
+        getCommand(paths, arg);
+        // std::cout << arg << ": not found\n";
+      }
     }
 
     else if (command == "exit" && input == "exit 0") break;
