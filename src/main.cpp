810c65d13e55e300defb9a857aa5aeda0711d05f
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2025-02-26 15:14:19 +0530

    implement type command

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <unordered_set>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -9,6 +10,8 @@ int main() {
   // Uncomment this block to pass the first stage
   std::cout << "$ ";
 
+  std::unordered_set<std::string> commands;
+  commands = {"type", "echo", "exit"};
   std::string input;
   while (true) {
     std::getline(std::cin, input);
@@ -20,6 +23,14 @@ int main() {
       std::getline(iss >> std::ws, argument);
       std::cout << argument << std::endl;
     }
+    else if(command == "type"){
+      std::string arg;
+      std::getline(iss >> std::ws, arg);
+      if(commands.find(arg) != commands.end()) 
+        std::cout << arg << " is a shell builtin" << std::endl;
+      else std::cout << arg << ": not found\n";
+    }
+
     else if (command == "exit" && input == "exit 0") break;
     else {
       std::cout << input << ": "  << "command not found\n";
