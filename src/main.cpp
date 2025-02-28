a9e539b5c129bec3734a6e43e6c0d728f7a3a32b
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2025-02-28 16:54:15 +0530

    implement feature to execute executables

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 #include <sstream>
 #include <filesystem>
+#include <vector>
 #include <unordered_set>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+
+std::string getPathEnv() {
+  char* path = std::getenv("PATH");
+  return path ? std::string(path) : "";
+}
+
+
+bool fileExists(const std::string& file){
+  return std::filesystem::exists(file) && std::filesystem::is_regular_file(file);
+}
+
+void runExecutable(std::vector<std::string>& commands){
+  if(commands.empty()) return;
+  char* cmd = commands[0].data();
+  std::vector<char*> argv;
+  for(auto& arg:commands){
+    argv.push_back(arg.data());
+  }
+  argv.push_back(nullptr);
+
+  if(fork() == 0) {
+    execvp(cmd, argv.data());
+    std::cerr << cmd << ": command not found\n";
+    exit(1);
+  }
+  wait(nullptr);
+}
 
 void getCommand(const std::string paths, std::string arg){
   std::string dir;
   std::stringstream ss(paths);
   
   while(std::getline(ss, dir, ':')){
-
     // search for the command in each dir from the path env var.
     for(const auto& entry: std::filesystem::directory_iterator(dir)){
       if(entry.path().filename() == arg) {
@@ -23,41 +53,45 @@ void getCommand(const std::string paths, std::string arg){
 }
 
 int main() {
-  // Flush after every std::cout / std:cerr
+  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   // Uncomment this block to pass the first stage
   std::cout << "$ ";
-
-  std::unordered_set<std::string> commands;
-  commands = {"type", "echo", "exit"};
+  
+  std::string paths = getPathEnv();
+  
+  std::unordered_set<std::string> built_in;
+  built_in = {"type", "echo", "exit"};
   std::string input;
   while (true) {
     std::getline(std::cin, input);
     std::istringstream iss(input);
     std::string command;
-    iss >> command;
-    if(command == "echo"){
+    std::vector<std::string> commands;
+    while(iss >> std::ws >> command){
+      commands.push_back(command);
+    }
+    if(commands[0] == "echo"){
       std::string argument;
       std::getline(iss >> std::ws, argument);
       std::cout << argument << std::endl;
     }
 
-    else if(command == "type"){
+    else if(commands[0] == "type"){
       std::string arg;
       std::getline(iss >> std::ws, arg);
-      if(commands.find(arg) != commands.end()) 
+      if(built_in.find(arg) != built_in.end()) 
         std::cout << arg << " is a shell builtin" << std::endl;
       else {
-        std::string paths = std::getenv("PATH");
         getCommand(paths, arg);
-        // std::cout << arg << ": not found\n";
       }
     }
 
     else if (command == "exit" && input == "exit 0") break;
+    
     else {
-      std::cout << input << ": "  << "command not found\n";
+      runExecutable(commands);
     }
     std::cout << "$ ";
   }
